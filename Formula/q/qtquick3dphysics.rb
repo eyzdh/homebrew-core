a81183b86635a6ed51c0fe8cd4584d33d2b272c9
eyzdh <100879419+eyzdh@users.noreply.github.com>
2025-11-11 22:48:40 +0100

    Update qtquick3dphysics.rb

--- Formula/q/qtquick3dphysics.rb
+++ Formula/q/qtquick3dphysics.rb
@@ -19,12 +19,12 @@ class Qtquick3dphysics < Formula
   depends_on "cmake" => :build
   depends_on "ninja" => :build
 
-  depends_on "qtbase"
-  depends_on "qtdeclarative"
-  depends_on "qtquick3d"
+  depends_on "eyzdh/core/qtbase"
+  depends_on "eyzdh/core/qtdeclarative"
+  depends_on "eyzdh/core/qtquick3d"
 
   on_macos do
-    depends_on "qtshadertools"
+    depends_on "eyzdh/core/qtshadertools"
   end
 
   # TODO: preserve_rpath # https://github.com/orgs/Homebrew/discussions/2823
