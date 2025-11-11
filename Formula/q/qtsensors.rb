708e9c05edb447ab4fd1d1f7c78f52d6ec873a72
eyzdh <100879419+eyzdh@users.noreply.github.com>
2025-11-11 22:46:55 +0100

    Update qtsensors.rb

--- Formula/q/qtsensors.rb
+++ Formula/q/qtsensors.rb
@@ -19,8 +19,8 @@ class Qtsensors < Formula
   depends_on "ninja" => :build
   depends_on "pkgconf" => :test
 
-  depends_on "qtbase"
-  depends_on "qtdeclarative"
+  depends_on "eyzdh/core/qtbase"
+  depends_on "eyzdh/core/qtdeclarative"
 
   # TODO: preserve_rpath # https://github.com/orgs/Homebrew/discussions/2823
 
