19c856a68f0216a98784a7634ce40934f87a36c3
eyzdh <100879419+eyzdh@users.noreply.github.com>
2025-11-11 22:49:19 +0100

    Update qtpositioning.rb

--- Formula/q/qtpositioning.rb
+++ Formula/q/qtpositioning.rb
@@ -21,9 +21,9 @@ class Qtpositioning < Formula
   depends_on "ninja" => :build
   depends_on "pkgconf" => :test
 
-  depends_on "qtbase"
-  depends_on "qtdeclarative"
-  depends_on "qtserialport"
+  depends_on "eyzdh/core/qtbase"
+  depends_on "eyzdh/core/qtdeclarative"
+  depends_on "eyzdh/core/qtserialport"
 
   # TODO: preserve_rpath # https://github.com/orgs/Homebrew/discussions/2823
 
